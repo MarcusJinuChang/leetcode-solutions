d873ea8c6f53e309404d4208eed45efb2d26fe68
Marcus Chang <changm2608@gmail.com>
2026-08-08 17:50:59 -0700

    Time: 2 ms (73.91%), Space: 42.5 MB (96.11%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if (s.equals("")) return true;
+        int p = 0;
+
+        for (int i = 0; i < t.length(); i++) {
+            if (s.charAt(p) == t.charAt(i)) {
+                p++;
+            }
+
+            if (p == s.length()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
