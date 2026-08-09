b6ba244f4f56bc454c57c2208537753dc724109b
Marcus Chang <changm2608@gmail.com>
2026-08-08 17:42:39 -0700

    Time: 3 ms (22.9%), Space: 43.4 MB (97.96%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,5 +1,34 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
         
+        for (int i = 0; i < m; i++) {
+            int in = nums1[i];
+            if (!map.keySet().contains(in)) {
+                map.put(in, 1);
+            } else {
+                map.put(in, map.get(in) + 1);
+            }
+        }
+
+        for (int in: nums2) {
+            if (!map.keySet().contains(in)) {
+                map.put(in, 1);
+            } else {
+                map.put(in, map.get(in) + 1);
+            }
+        }
+
+        ArrayList<Integer> ar = new ArrayList<>();
+
+        for (int in: map.keySet()) {
+            for (int i = 0; i < map.get(in); i++) {
+                ar.add(in);
+            }
+        }
+
+        for (int i = 0; i < ar.size(); i++) {
+            nums1[i] = ar.get(i);
+        }
     }
 }
